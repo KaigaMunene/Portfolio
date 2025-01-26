8df8d7acf3ac6249041eb3904fe52335c7692ad5
KaigaMunene <kaigamunene@gmail.com>
2025-01-26 22:38:54 +0300

    feat: create hero page * create hero page * style and make it responsive * make changes to navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Navbar from './components/NavBar';
+import HeroSection from './pages/Hero';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <HeroSection />
     </div>
   );
 }
